0c747991a6dcb53c469c849176d363f793057d45
David Culbreth <dculbreth@mail.stmarytx.edu>
2018-10-01 20:49:27 -0500

    Adding arrays

--- __init__.py
+++ __init__.py
@@ -6,35 +6,73 @@ import pprint
 class CFGObj:
 
   def __init__(self, *args, **kwargs):
+    if len(args) > 0:
+      raise ValueError("Bare arrays cannot be entered into the CFGObject.")
     self.__dict__ = dict()
     if isinstance(kwargs, dict):
       self.__dict__.update(copy.deepcopy(kwargs))
     for key, value in self.__dict__.items():
       if isinstance(value, dict):
-        self.__dict__[key] = self.__class__(**value)
+        self.__dict__[key] = self.__class__.from_dict(value)
+      if isinstance(value, list):
+        self.__dict__[key] = self.__class__.from_array(value)
+      
   @classmethod
   def from_json_file(cls, filepath):
     with open(filepath) as json_file:
       values = json.loads(json_file.read())
-      output = cls(**values)
+      output = cls.from_dict_or_array(values)
     return output
   
   @classmethod
   def from_yaml_file(cls, filepath):
     with open(filepath) as yaml_file:
       values = yaml.load(yaml_file.read())
-      output = cls(**values)
+      output = cls.from_dict_or_array(values)
     return output
 
   @classmethod
   def from_json_str(cls, json_str):
     values = json.loads(json_str)
-    output = cls(**values)
+    output = cls.from_dict_or_array(values)
     return output
 
   @classmethod
   def from_dict(cls, dictionary):
-    return cls(**dictionary)
+    if isinstance(dictionary, dict):
+      return cls(**dictionary)
+    else:
+      raise ValueError("CFGObj.from_dict() called with non-dict object.")
+  
+  @classmethod
+  def from_array(cls, array):
+    if isinstance(array, list) or isinstance(array, tuple):
+      items = list()
+      for item in array:
+        items.append(cls.from_generic_value(item))
+      return items
+    else:
+      raise ValueError("CFGObj.from_array() called with non-dict object.")
+
+  @classmethod
+  def from_generic_value(cls, value):
+    if value.__class__ in (int, float, str):
+      return value
+    elif value.__class__ in (dict, list, tuple):
+      return cls.from_dict_or_array(value)
+    elif hasattr(value, "__dict__"):
+      return cls.from_dict(value.__dict__)
+    else:
+      raise ValueError("Unknown value in `from_generic_value(...): {0} : {1}`. How did you get here?".format(str(value), value.__class__))
+
+  @classmethod
+  def from_dict_or_array(cls, obj):
+    if isinstance(obj, dict):
+      return cls.from_dict(obj)
+    elif isinstance(obj, list) or isinstance(obj, tuple):
+      return cls.from_array(obj)
+    else: # if it's non-iterable, just extract values from its __dict__
+      raise ValueError("Unknown value in `from_dict_or_array(...): {0} : {1}` How did you get here?".format(str(obj), obj.__class__))
   
   def to_json_str(self) -> str :
     return json.dumps(self.__dict__, indent=2)
--- tests/test_CFGObj.py
+++ tests/test_CFGObj.py
@@ -5,6 +5,7 @@ import cfgutils
 class test_CFGObj(unittest.TestCase):
   def runTest(self):
     self.test_JSON_Import()
+    
 
   def setUp(self):
     print("Running {0}.setUp()".format(self.__class__.__name__))
@@ -13,7 +14,24 @@ class test_CFGObj(unittest.TestCase):
     print("Running {0}.tearDown()".format(self.__class__.__name__))
 
   def test_JSON_Import(self):
-    self.assertTrue(True)
+    self.test_JSON_Nested_Objects()
+    self.test_JSON_Nested_Arrays()
+
+  def test_JSON_Nested_Arrays(self):
+    testJSONPath = os.path.join(os.path.dirname(__file__), "data", "testArrayData.json")
+    testObj = cfgutils.CFGObj.from_json_file(testJSONPath)
+    
+    print("testObj:", str(testObj))
+    self.assertNotEqual(testObj, None)
+    self.assertEqual(testObj[0].a, 123)
+    self.assertEqual(testObj[1].b, 456)
+    self.assertEqual(testObj[2].c, 789)
+    self.assertEqual(testObj[2].d[0], 'a')
+    self.assertEqual(testObj[2].d[1], 'b')
+    self.assertEqual(testObj[2].d[2], 'c')
+    self.assertEqual(testObj[2].d[3], 'd')
+
+  def test_JSON_Nested_Objects(self):
     testJSONPath = os.path.join(os.path.dirname(__file__), "data", "testData.json")
     testObj = cfgutils.CFGObj.from_json_file(testJSONPath)
 
